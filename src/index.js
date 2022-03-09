0a6f7f557e8ae88f4e4f6cb0a452ff5d79aea6cd
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-09 08:59:24 +0100

    feat: added redux store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import userReducer from './features/user.js'
+
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  //Add your redux features here.
+  reducer: {
+   // user: userReducer
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={(store)}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
