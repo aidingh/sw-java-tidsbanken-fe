669c0c9770ce98b14d73003f83f1800ece1d9d82
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-09 11:53:42 +0100

    feat: add the calendar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import CalendarView from './views/CalendarView';
 
 /**
  * @return {Component} The app component
  */
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className='h-screen w-screen'>
+      <CalendarView />
+    </div>
   );
 }
 
