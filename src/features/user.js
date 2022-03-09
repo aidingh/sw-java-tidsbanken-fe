69be51e823c1582efa7a158cb237b668755e1c70
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-09 09:00:23 +0100

    feat: example of a redux feature This shows how to setup the action and reducer.

---
+++ src/features/user.js
@@ -0,0 +1,28 @@
+
+/**
+ * This is just an example of how u can set up your redux reducer.
+ * Just copy the boilerplate code down below and add it to your own redux feature.
+ */
+
+// import {createSlice} from "@reduxjs/toolkit"
+
+// const initialStateValue = {name: "", age: null, email: ""}
+
+// export const userSlice = createSlice({
+//     name: "user", // the name of the reducer
+//     initialState: {value: initialStateValue}, // initial state
+//     reducers: {
+        // You create your different redux reducers here.
+//         login: (state, action) =>{
+//             state.value = action.payload
+//         },
+//         logout: (state) => {
+//             state.value = initialStateValue
+//         }
+//     }
+// })
+
+// export const  {login, logout} = userSlice.actions  // This is where you export your actions
+
+
+// export default userSlice.reducer
