b6d36360f942b564924e219d4e3468d605e90762
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-10 13:21:27 +0100

    feat: add formInputComponent reusable input component

---
+++ src/components/FormInputComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Form input component
+ * @param {handleChange} handleChange - Use to handle changes to input form.
+ * @param {label} label - The label of the form e.g name, email etc.
+ * @param {otherProps} otherProps - Props to make the input field unique.
+ * @return {Component} returns then FormInputComponent
+ */
+const FormInputComponent = ({handleChange, label}) => {
+  return (
+    <div className='relative m-4'>
+      <input placeholder="Email" className='peer h-10
+       w-48 border-b-2 border-gray-300
+       text-gray-900 placeholder-transparent
+        focus:outline-none focus:border-blue-500'
+      onChange={handleChange} />
+      <label className='absolute left-0 -top-3.5
+       text-gray-600 text-sm transition-all peer-placeholder-shown:text-base
+        peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
+        peer-focus:-top-3.5 peer-focus:text-blue-500
+        peer-focus:text-sm'>{label}</label>
+    </div>
+  );
+};
+
+
+FormInputComponent.propTypes = {
+  handleChange: PropTypes.func,
+  label: PropTypes.string,
+  otherProps: PropTypes.any,
+};
+
+export default FormInputComponent;
