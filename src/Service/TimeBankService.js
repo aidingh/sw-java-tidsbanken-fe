7a4128263e807e49ffdd46fcd44a895b28d06c7d
aidingh <aidingh@hotmail.com>
2022-03-10 15:56:31 +0100

    FEAT: Service class to get data with JWT token. Page navigation. Home and Login page.

---
+++ src/Service/TimeBankService.js
@@ -0,0 +1,48 @@
+import { Component } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export class TimeBankService extends Component {
+  
+  static timeBankInstance = null;
+
+
+  static getInstance() {
+    return new TimeBankService();
+  }
+
+  async getUserData(jwt) {
+
+    try {
+      let userResponse = fetch(`${"http://localhost:8080/api/v1/users"}`, {headers:{ "authorization": `Bearer ${jwt}`, "Content-Type": "application/json" }});
+      if ((await userResponse).ok) {
+        let data = (await userResponse).json();
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+  async postUserData(jwt) {
+    try {
+      let response = fetch(`${"some val"}/translations/${jwt}`, {
+        method: "POST",
+        headers: { "Authorization": `Bearer ${jwt}`, "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: "some id",
+          translations: "yo",
+        }),
+      });
+  
+      if ((await response).ok) {
+        let data = (await response).json();
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+}
+export default TimeBankService;
