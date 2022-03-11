5653dec6bcb637ed3b5d2ed34f5de85a7315b0fc
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-11 15:19:48 +0100

    fix:removed a comma from the render

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {Auth0Provider} from '@auth0/auth0-react';
 import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
 
-const homeUrl = "http://localhost:3000/home"
+const homeUrl = 'http://localhost:3000/home';
 
 const store = configureStore({
   // Add your redux features here.
@@ -18,18 +18,16 @@ const store = configureStore({
 
 ReactDOM.render(
     <React.StrictMode>
-      
-        <Auth0Provider
-          domain="dev-377qri7m.eu.auth0.com"
-          clientId="5F9MNB2qBN9dR6JxAdwF2zjVKic8ZBwh"
-          redirectUri={homeUrl}
-          audience="https://time.bank.com"
-        >
-          <Provider store={(store)}>
+      <Auth0Provider
+        domain="dev-377qri7m.eu.auth0.com"
+        clientId="5F9MNB2qBN9dR6JxAdwF2zjVKic8ZBwh"
+        redirectUri={homeUrl}
+        audience="https://time.bank.com"
+      >
+        <Provider store={(store)}>
           <App />
-          </Provider>
-        </Auth0Provider>,
-      
+        </Provider>
+      </Auth0Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
