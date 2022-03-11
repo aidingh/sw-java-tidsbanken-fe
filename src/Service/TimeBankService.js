178e6db81626ee3cf1c8926fdbc7e0f371fdfa8d
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-11 09:27:50 +0100

    feat: custom hook to fetch data. Changed from using a class to using functions

--- src/Service/TimeBankService.js
+++ src/Service/TimeBankService.js
@@ -1,48 +1,61 @@
-import { Component } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+// import {useAuth0} from '@auth0/auth0-react';
 
-export class TimeBankService extends Component {
-  
-  static timeBankInstance = null;
+/**
+ *
+ * To use this custom fetch hook import and call
+ * const { data, error, isLoaded } = useApiRequest(url);
+ * @param {String} url The API url and endpoint
+ * @param {Object} options Headers and so forth
+ * @return {Array} The data you want to fetch
+ * @return {Boolean} If the data has loaded or not
+ * @return {String} Error message
+ */
+export const useApiRequest = (url, options) => {
+  const [data, setData] = React.useState([]);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
-
-  static getInstance() {
-    return new TimeBankService();
-  }
-
-  async getUserData(jwt) {
-
-    try {
-      let userResponse = fetch(`${"http://localhost:8080/api/v1/users"}`, {headers:{ "authorization": `Bearer ${jwt}`, "Content-Type": "application/json" }});
-      if ((await userResponse).ok) {
-        let data = (await userResponse).json();
-        return data;
+  React.useEffect(() => {
+    /**
+     * function to fetch data.
+     */
+    const getData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const json = await json(response);
+        setData(json);
+        setIsLoaded(true);
+      } catch (error) {
+        setError(console.error());
       }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
+    };
+    getData();
+  }, [url]);
 
-  async postUserData(jwt) {
-    try {
-      let response = fetch(`${"some val"}/translations/${jwt}`, {
-        method: "POST",
-        headers: { "Authorization": `Bearer ${jwt}`, "Content-Type": "application/json" },
-        body: JSON.stringify({
-          id: "some id",
-          translations: "yo",
-        }),
-      });
-  
-      if ((await response).ok) {
-        let data = (await response).json();
-        return data;
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  return {error, isLoaded, data};
+};
 
-}
-export default TimeBankService;
+/**
+ * A function to POST data
+ * @param {String} url The API url with endpoint
+ * @param {Object} body The data you want to post
+ * @return {Object} The data that is posted
+ */
+export const postData = async (url, body) => {
+  try {
+    const response = fetch(`${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if ((await response).ok) {
+      const data = (await response).json();
+      return data;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
