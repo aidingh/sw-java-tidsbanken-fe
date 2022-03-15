8dc467f15e624d6aafddce55219405bfbe37ba67
Love Beling <love.beling@se.experis.com>
2022-03-15 11:31:01 +0100

    feat(CalenderView): added colors for different vacation requests

--- src/Components/CalendarView.js
+++ src/Components/CalendarView.js
@@ -1,36 +1,67 @@
-import {React, useEffect} from 'react';
+import {React, useEffect, useState} from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
-
 /**
  *  @return {Component} The CalendarView component
  */
 const CalendarView = () => {
+  const [eventData, setEventData] = useState([]);
+
   useEffect(async () => {
     const data = await fetch('http://localhost:80/vacation');
     const jsonData = await data.json();
 
-    console.log(jsonData);
-  });
+    const events = [];
+    jsonData.forEach((vacationRequest) => {
+      if ('APPROVED'.localeCompare(vacationRequest.status) == 0) {
+        events.push({
+          status: vacationRequest.status,
+          title: vacationRequest.title,
+          start: vacationRequest.startPeriod,
+          end: vacationRequest.endPeriod,
+          color: '#00cc00',
+          textColor: '#000000',
+        });
+      }
 
-  const eventData = [
-    {
-      title: 'heeeej',
-      start: '2022-03-02',
-    },
-    {
-      title: 'heeeej',
-      start: '2022-03-05',
-    },
-  ];
+      if ('DENIED'.localeCompare(vacationRequest.status) == 0) {
+        events.push({
+          status: vacationRequest.status,
+          title: vacationRequest.title,
+          start: vacationRequest.startPeriod,
+          end: vacationRequest.endPeriod,
+          color: '#800000',
+          textColor: '#000000',
+        });
+      }
+
+      if ('PENDING'.localeCompare(vacationRequest.status) == 0) {
+        events.push({
+          status: vacationRequest.status,
+          title: vacationRequest.title,
+          start: vacationRequest.startPeriod,
+          end: vacationRequest.endPeriod,
+          color: '#ffff80',
+          textColor: '#000000',
+        });
+      }
+    });
+
+
+    setEventData(events);
+
+    console.log(events);
+  }, []);
 
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView="dayGridMonth"
       events={eventData}
-    />
+    >
+    </FullCalendar>
   );
 };
+
 
 export default CalendarView;
