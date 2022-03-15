e0fdc15d37bf8f99f8d8ec79353be55fd7ff2bf2
Love Beling <love.beling@se.experis.com>
2022-03-15 09:35:48 +0100

    feat(CalenderView): added temporary events to calender and added some fetch logic

--- src/Components/CalendarView.js
+++ src/Components/CalendarView.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
+import {React, useEffect} from 'react';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
 /**
  *  @return {Component} The CalendarView component
  */
 const CalendarView = () => {
+  useEffect(async () => {
+    const data = await fetch('http://localhost:80/vacation');
+    const jsonData = await data.json();
+
+    console.log(jsonData);
+  });
+
+  const eventData = [
+    {
+      title: 'heeeej',
+      start: '2022-03-02',
+    },
+    {
+      title: 'heeeej',
+      start: '2022-03-05',
+    },
+  ];
+
   return (
-    <>
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        height="100%"
-      />
-    </>
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridMonth"
+      events={eventData}
+    />
   );
 };
 
