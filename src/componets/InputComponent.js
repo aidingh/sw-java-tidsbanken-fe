287e31eb5bb5d70cc39aeecec78abbdf2ab5ab69
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-16 10:07:32 +0100

    feat: Input can now be disabled and show a value

--- src/componets/InputComponent.js
+++ src/componets/InputComponent.js
@@ -8,14 +8,24 @@ import PropTypes from 'prop-types';
  * @param {type} type The type of the input field.
  * @return {Component} returns then FormInputComponent
  */
-const FormInputComponent = ({handleChange, label, type}) => {
+const FormInputComponent = ({handleChange, label, type, isDisabled, value}) => {
   return (
     <div className='relative'>
-      <input type={type} id={label} placeholder={label} className='peer h-10
-       w-48 border-b-2 border-gray-300
+      {isDisabled ?
+       <input disabled value={value}
+         type={type} id={label} placeholder={label} className='
+       h-10
+       w-64 border-b-2 border-gray-300
        text-gray-900 placeholder-transparent
-        focus:outline-none focus:border-blue-500'
-      onChange={handleChange} />
+        focus:outline-none focus:border-blue-500
+        disabled:select-none disabled:bg-white'
+         onChange={handleChange} /> :
+       <input type={type} id={label} placeholder={label} className='
+      peer h-10
+       w-64 border-b-2 border-gray-300
+       text-gray-900 placeholder-transparent
+        focus:outline-none focus:border-blue-500 disabled:select-none'
+       onChange={handleChange} />}
       <label htmlFor={label} className='absolute left-0 -top-3.5
        text-gray-600 text-sm transition-all peer-placeholder-shown:text-base
         peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
@@ -30,6 +40,8 @@ FormInputComponent.propTypes = {
   handleChange: PropTypes.func,
   label: PropTypes.string,
   type: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  value: PropTypes.string,
 };
 
 export default FormInputComponent;
