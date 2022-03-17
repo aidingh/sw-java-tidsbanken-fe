25b44d10d9a30175ad6aa07ff051d74478ac6d24
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-17 15:16:31 +0100

    style: fixed indentations

--- src/Components/InputComponent.js
+++ src/Components/InputComponent.js
@@ -8,24 +8,24 @@ import PropTypes from 'prop-types';
  * @param {type} type The type of the input field.
  * @return {Component} returns then FormInputComponent
  */
-const FormInputComponent = ({handleChange, label, type, isDisabled, value}) => {
+const FormInputComponent = ({ handleChange, label, type, isDisabled, value }) => {
   return (
     <div className='relative'>
       {isDisabled ?
-       <input disabled value={value}
-         type={type} id={label} placeholder={label} className='
+        <input disabled value={value}
+          type={type} id={label} placeholder={label} className='
        h-10
        w-64 border-b-2 border-gray-300
        text-gray-900 placeholder-transparent
         focus:outline-none focus:border-blue-500
         disabled:select-none disabled:bg-white'
-         onChange={handleChange} /> :
-       <input type={type} id={label} placeholder={label} className='
+          onChange={handleChange} /> :
+        <input type={type} id={label} placeholder={label} className='
       peer h-10
        w-64 border-b-2 border-gray-300
        text-gray-900 placeholder-transparent
         focus:outline-none focus:border-blue-500 disabled:select-none'
-       onChange={handleChange} />}
+          onChange={handleChange} />}
       <label htmlFor={label} className='absolute left-0 -top-3.5
        text-gray-600 text-sm transition-all peer-placeholder-shown:text-base
         peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2
