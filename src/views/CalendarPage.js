08778eb99216bc9dd37305f4b66d86fcd0e6ff8c
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-17 11:33:43 +0100

    feat: changed to generic fetch

--- src/views/CalendarPage.js
+++ src/views/CalendarPage.js
@@ -1,42 +1,42 @@
 import { React, useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-import ModalComponent from '../Components/ModalComponent'
-import {useSelector} from "react-redux";
-import {useAuth0} from "@auth0/auth0-react";
+import ModalComponent from '../Components/ModalComponent';
+import { useSelector } from "react-redux";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useFetch } from '../Service/TimeBankService';
+
+
 /**
  *  @return {Component} The CalendarView component
  */
 const CalendarView = () => {
-
-  
-
   const [eventData, setEventData] = useState([]);
   const [open, setOpen] = useState(false);
   const state = useSelector((state) => state.token_reducer.value);
+  const bearer = `Bearer ${state.jwt_token}`;
 
-  const {user} = useAuth0();
+
+  const { user } = useAuth0();
   let split = user.sub.split("|");
 
   const userId = split[1];
   const admin = state.role;
 
-     /**
-   * Open and closes the modal.
-   * @return {Boolean}
-   */
-      const handleOpen = () => {
-        setOpen(!open)
-      }
+
+  /**
+* Open and closes the modal.
+* @return {Boolean}
+*/
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
 
   useEffect(async () => {
     const events = [];
-
     if (admin == "Admin") {
-      const vacations = await fetch(
-        "http://localhost:8080/api/v1/vacation/all"
-      );
-      const vacationsJson = await vacations.json();
+      let vacationsJson = await useFetch("http://localhost:8080/api/v1/vacation/all", bearer);
       vacationsJson.forEach((vacation) => {
         if ("APPROVED".localeCompare(vacation.status) == 0) {
           setSingleVacationRequest("#00cc00", vacation, events);
@@ -50,36 +50,36 @@ const CalendarView = () => {
           setSingleVacationRequest("#ffff80", vacation, events);
         }
       });
-    } else {
-      const approvedVacations = await fetch(
-        "http://localhost:8080/api/v1/vacation/approved"
-      );
-      const approvedVacationsJson = await approvedVacations.json();
+    }
+    else {
+      const approvedVacationsJson = await useFetch("http://localhost:8080/api/v1/vacation/approved", bearer);
       approvedVacationsJson.forEach((vacationRequest) => {
         setSingleVacationRequest("#00cc00", vacationRequest, events);
       });
 
-      const userVacationRequests = await fetch(
-        "http://localhost:8080/api/v1/vacation/" + userId
-      );
-      const userVacationRequestsJson = await userVacationRequests.json();
+      const userVacationRequestsJson = await useFetch(`http://localhost:8080/api/v1/vacation/${userId}`, bearer);
+      try {
+        userVacationRequestsJson.forEach((vacation) => {
+          if ("APPROVED".localeCompare(vacation.status) == 0) {
+            setSingleVacationRequest("#00cc00", vacation, events);
+          }
 
-      userVacationRequestsJson.forEach((vacation) => {
-        if ("APPROVED".localeCompare(vacation.status) == 0) {
-          setSingleVacationRequest("#00cc00", vacation, events);
-        }
+          if ("DENIED".localeCompare(vacation.status) == 0) {
+            setSingleVacationRequest("#800000", vacation, events);
+          }
 
-        if ("DENIED".localeCompare(vacation.status) == 0) {
-          setSingleVacationRequest("#800000", vacation, events);
-        }
+          if ("PENDING".localeCompare(vacation.status) == 0) {
+            setSingleVacationRequest("#ffff80", vacation, events);
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
 
-        if ("PENDING".localeCompare(vacation.status) == 0) {
-          setSingleVacationRequest("#ffff80", vacation, events);
-        }
-      });
     }
     setEventData(events);
   }, []);
+
 
   function setSingleVacationRequest(color, vacationRequest, events) {
     events.push({
@@ -99,17 +99,18 @@ const CalendarView = () => {
         initialView="dayGridMonth"
         events={eventData}
         customButtons={{
-          vacationRequest:{
-            text:'Vacation Request',
+          vacationRequest: {
+            text: 'Vacation Request',
             click: () => {
-              handleOpen()
+              handleOpen();
             }
           }
         }}
-        headerToolbar={{center: 'vacationRequest'}}
+        headerToolbar={{ center: 'vacationRequest' }}
       >
       </FullCalendar>
       <ModalComponent handleOpen={handleOpen} open={open} />
+
     </>
   );
 };
