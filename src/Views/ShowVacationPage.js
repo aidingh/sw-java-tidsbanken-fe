c90668c66ee1e28ec54e858c2ad2afb2bf07e4d0
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-18 11:21:43 +0100

    feat: add showVacationPage

---
+++ src/Views/ShowVacationPage.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import FormInputComponent from '../Components/InputComponent';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import TextField from '@mui/material/TextField';
+
+
+const ShowVacationPage = ({ vacationTitle, vacationStartDate, vacationEndDate }) => {
+    const { user } = useAuth0();
+    return (
+        <form className="w-96 h-96 bg-white m-auto flex flex-col">
+            <div className='self-center mt-6'>
+                <FormInputComponent isDisabled
+                    label='Name' type='text' value={user.nickname} />
+            </div>
+            <div className='self-center mt-6'>
+                <FormInputComponent isDisabled
+                    label='Vacation request title' type='text' value={vacationTitle} />
+            </div>
+            <div className='self-center my-3'>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                        readOnly
+                        value={vacationStartDate}
+                        onChange={() => {
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
+            </div>
+            <div className='self-center my-3'>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <DatePicker
+                        readOnly
+                        value={vacationEndDate}
+                        onChange={() => {
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                    />
+                </LocalizationProvider>
+            </div>
+            <div className='flex justify-around content-end grow items-center'>
+                <button className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-6 rounded justify-self-center'
+                >Accept</button>
+                <button className='bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-6 rounded justify-self-center'
+                >Deny</button>
+                <button className='bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-6 rounded justify-self-center'
+                >Delete</button>
+            </div>
+        </form>
+    );
+};
+
+export default ShowVacationPage;
