a2706950a754cf7b7adc49da3df709c517464f48
Love Beling <love.beling@se.experis.com>
2022-03-22 09:45:17 +0100

    feat: added first, last name and email for logged in user

--- src/Views/ProfilePage.js
+++ src/Views/ProfilePage.js
@@ -4,6 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LoadingSpinner from "../Elements/LoadingSpinner";
 import { Navigate } from "react-router-dom";
 import ApplicationFrame from "../Elements/ApplicationFrame";
+import InputComponent from "../Components/InputComponent"
 function ProfilePage() {
 
   const { logout } = useAuth0();
@@ -27,22 +28,25 @@ function ProfilePage() {
     {isAuthenticated && (
       <>
         <ApplicationFrame startLogOutAction={startLogOutAction}/>
-        <div className="flex flex-col justify-center">
-          <div className="flex flex-wrap justify-center">
-            <div className="w-6/12 sm:w-4/12 px-4">
+        <div className="w-96 h-96 pt-10 bg-white m-auto flex flex-col">
+          <div className="w-6/12 sm:w-4/12 px-4 self-center mt-6">
               <img
                 src={user.picture}
                 alt="..."
                 className="shadow-lg rounded max-w-full h-auto align-middle border-none"
               />
-              <h2>{user.nickname}</h2>
-              <p>{user.email}</p>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-              >
-                Get user data
-              </button>
-            </div>
+          </div>
+
+          <div className='self-center mt-6'>
+            <InputComponent isDisabled={true} label="First Name" type="text" value={"Love"} />
+          </div>
+
+          <div className='self-center mt-6'>
+            <InputComponent isDisabled={true} label="Last Name" type="text" value={"Beling"} />
+          </div>
+
+          <div className='self-center mt-6'>
+            <InputComponent isDisabled={true} label="Email" type="text" value={"Beling"} handleChange={() => console.log("hej")}/>
           </div>
         </div>
       </>
