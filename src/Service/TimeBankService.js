98acbe2d3ae3a511293e5300beaeca85a1002f41
Simon Palmgren Arvidsson <simon.palmgrenarvidsson@se.experis.com>
2022-03-23 11:09:22 +0100

    refactor: removed not used variable

--- src/Service/TimeBankService.js
+++ src/Service/TimeBankService.js
@@ -18,8 +18,8 @@ export const useFetch = async (url, bearer, successCallback) => {
     const data = await response.json();
 
     successCallback?.();
-    return data
-    
+    return data;
+
   } catch (error) {
     console.error(error);
   }
@@ -44,14 +44,14 @@ export const postData = async (url, bearer, body, successCallback) => {
       },
       body: JSON.stringify(body),
     });
-    
+
     if ((await response).ok) {
       const data = (await response).json();
       successCallback?.();
       return data;
     }
 
-    console.error(await (await response).text())
+    console.error(await (await response).text());
   } catch (error) {
     console.error(error);
   }
@@ -76,39 +76,19 @@ export const patchData = async (url, bearer, body, successCallback) => {
       },
       body: JSON.stringify(body),
     });
-    
+
     if ((await response).ok) {
       const data = (await response).json();
       successCallback?.();
       return data;
     }
 
-    console.error(await (await response).text())
+    console.error(await (await response).text());
   } catch (error) {
     console.error(error);
   }
 };
 
-export const patchData = async (url, bearer, body) => {
-  try {
-    const response = fetch(url, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Authorization': bearer
-      },
-      body: JSON.stringify(body),
-    });
-
-    if ((await response).ok) {
-      const data = (await response).json();
-      return data;
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 export const deleteData = async (url, bearer) => {
   try {
